11e97621a378a61ec61d8bcb129fce8ad594dec5
Akarin Sangyor <specialday191@gmail.com>
2025-06-02 21:52:24 +0700

    [fe-update] : fix the header

--- frontend/components/TheHeader.vue
+++ frontend/components/TheHeader.vue
@@ -6,26 +6,28 @@
                 <img src="@/assets/images/avatar.jpg" alt="Avatar"
                     class="rounded-full w-20 h-20 border-2 border-primary md:w-52 md:h-52 md:mb-4" />
             </div>
-        </div>
 
-        <div>
-            <div class="font-bold text-xl text-center md:text-2xl">
-                Akarin Sangyor
-            </div>
-            <p class="text-base text-center md:text-xl">
-                Never Stop Learning!
-            </p>
-            <div class="pt-2 flex flex-wrap gap-2 justify-center md:mt-2">
-                <BaseLink size="small" to="mailto: Akarin.Sangyor@gmail.com"
-                    class="md:text-base md:px-7 md:py-3 md:space-x-2">
-                    <span>Email</span>
-                    <IconEnvelope class="inline-block w3 md:w-4"> </IconEnvelope>
-                </BaseLink>
-                <BaseLink size="small" to="https://www.linkedin.com/in/akarin-sangyor-112282258/?originalSubdomain=th"
-                    class="md:text-base md:px-7 md:py-3 md:space-x-2" target="blank">
-                    <span>Linkedin</span>
-                    <IconLinkedin class="inline-block w3 md:w-4"> </IconLinkedin>
-                </BaseLink>
+
+            <div>
+                <div class="font-bold text-xl text-center md:text-2xl">
+                    Akarin Sangyor
+                </div>
+                <p class="text-base text-center md:text-xl">
+                    Never Stop Learning!
+                </p>
+                <div class="pt-2 flex flex-wrap gap-2 justify-center md:mt-2">
+                    <BaseLink size="small" to="mailto: Akarin.Sangyor@gmail.com"
+                        class="md:text-base md:px-7 md:py-3 md:space-x-2">
+                        <span>Email</span>
+                        <IconEnvelope class="inline-block w3 md:w-4"> </IconEnvelope>
+                    </BaseLink>
+                    <BaseLink size="small"
+                        to="https://www.linkedin.com/in/akarin-sangyor-112282258/?originalSubdomain=th"
+                        class="md:text-base md:px-7 md:py-3 md:space-x-2" target="blank">
+                        <span>Linkedin</span>
+                        <IconLinkedin class="inline-block w3 md:w-4"> </IconLinkedin>
+                    </BaseLink>
+                </div>
             </div>
         </div>
     </header>
