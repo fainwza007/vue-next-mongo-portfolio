f292bc43be001874fceb9286d6f6f744ee024dea
Akarin Sangyor <specialday191@gmail.com>
2025-06-16 21:38:14 +0700

    [be-update] : Create Repository (Profile)

--- backend/di/repositories.js
+++ backend/di/repositories.js
@@ -1,10 +1,12 @@
 import connect from "#app/repositories/mongo/connection";
 import useUserRepo from "#app/repositories/mongo/user";
+import useProfileRepo from "#app/repositories/mongo/profile";
 
 export default ({ db }) => {
   const connection = connect(db.mongo.uri);
 
   const userRepo = useUserRepo(connection);
+  const profileRepo = useProfileRepo(connection);
 
-  return { userRepo };
+  return { userRepo, profileRepo };
 };
---
+++ backend/repositories/mongo/profile.js
@@ -0,0 +1,72 @@
+import { Schema } from "mongoose";
+
+export default (connection) => {
+  const ProfileModel = connection.model(
+    "Profile",
+    new Schema({
+      overallSkills: [String],
+      experiences: [
+        {
+          title: String,
+          logo: String,
+          company: String,
+          type: {
+            type: String,
+          },
+          start: Date,
+          end: Date,
+          skills: [String],
+          detail: String,
+        },
+      ],
+    })
+  );
+
+  function toProfile(profile) {
+    return {
+      id: profile.id,
+      overallSkills: profile.overallSkills,
+      experiences: profile.experiences,
+    };
+  }
+
+  async function getProfile() {
+    let profile;
+
+    try {
+      profile = await ProfileModel.where({}).findOne();
+
+      if (!profile) {
+        return profile;
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+
+    return toProfile(profile);
+  }
+
+  async function updatedProfile(newProfile) {
+    let profile;
+
+    try {
+      const update = {
+        $set: newProfile,
+      };
+
+      profile = await ProfileModel.findOneAndUpdate({}, update, {
+        new: true,
+        upsert: true,
+      });
+    } catch (error) {
+      throw new Error(error);
+    }
+
+    return toProfile(profile);
+  }
+
+  return {
+    getProfile,
+    updatedProfile,
+  };
+};
